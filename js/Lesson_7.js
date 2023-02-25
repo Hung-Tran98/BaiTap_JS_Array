fec54709de85be95be227a212df3cc73b0c9ef0e
HungTran98 <hungtranitvn@gmail.com>
2023-02-25 18:33:06 +0700

    add link js for index.html

--- js/Lesson_7.js
+++ js/Lesson_7.js
@@ -1,14 +1,12 @@
-function sortUp(a, b) {
-  return a - b;
-}
 var numArrayCopy = [];
 function numberSort() {
   for (var i = 0; i < numArray.length; i++) {
     numArrayCopy.push(numArray[i]);
   }
   document.querySelector("#txtTotal7").innerHTML =
-    "Sắp xếp mảng tăng dần :" + numArrayCopy.sort(sortUp);
-  console.log(numArrayCopy);
-  console.log(numArray);
+    "Sắp xếp mảng tăng dần :" +
+    numArrayCopy.sort(function sortUp(a, b) {
+      return a - b;
+    });
 }
 document.querySelector("#btnNumSort").onclick = numberSort;
